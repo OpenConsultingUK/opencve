e1bd1a307437132a683b2cf55f259cbda98462b3
Nebula-London <rmgondalia7@gmail.com>
2025-02-25 20:59:18 +0000

    fetch the file from transfer directory

--- scheduler/dags/includes/operators/fetch_operator.py
+++ scheduler/dags/includes/operators/fetch_operator.py
@@ -14,6 +14,10 @@ class GitFetchOperator(KindOperator):
 
         # Check if remotes is well configured
         repo = git.Repo(repo_path)
+        # Set Git user name and email
+        repo.config_writer().set_value("user", "name", "Your Name").release()
+        repo.config_writer().set_value("user", "email", "you@example.com").release()
+
         remotes = repo.remotes
         if not remotes:
             raise AirflowException(f"Repository {repo_path} has no remote")
@@ -24,8 +28,23 @@ class GitFetchOperator(KindOperator):
 
         self.log.info(f"Fetching last changes from {repo_path}...")
         tracking_branch = repo.active_branch.tracking_branch()
-        repo.remotes.origin.fetch()
-        repo.git.reset("--hard", tracking_branch.name)
+        try:
+            # Apply patches
+            folder_path = f"/transfer_received/{pathlib.Path(repo_path).name}"
+            patch_files = sorted(pathlib.Path(folder_path).glob("**/*.patch"))
+            if not patch_files:
+                self.log.warning(f"No patch files found in {folder_path}")
+                return
+            for patch_file in patch_files:
+                self.log.info(f"Applying patch {patch_file}")
+                repo.git.am(str(patch_file))
+                try:
+                    patch_file.rename(patch_file.with_suffix('.patch.applied'))
+                    # patch_file.unlink()  # Delete the file already processed
+                except OSError:
+                    self.log.warning(f"Could not rename {patch_file}")
+        except git.GitCommandError:
+            self.log.exception("Error while fetching")
 
         if last_commit == repo.head.commit:
             self.log.info("No change detected")
